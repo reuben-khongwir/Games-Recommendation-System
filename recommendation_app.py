4ef4938b2e9fb524f272135435d15eae1bff7ec3
reuben-khongwir <reubenkhongwir@gmail.com>
2024-11-27 09:00:35 +0530

    recommendation_app.py

--- recommendation_app.py
+++ recommendation_app.py
@@ -18,24 +18,26 @@ def recommend_hybrid(game_name):
     matched_game, score, _ = process.extractOne(game_name, content_df['name'])
     if score < 90:
         return None, None
-
-    # Content-based recommendations
-    content_index = content_df[content_df['name'] == matched_game].index[0]
-    content_distances, content_indices = content_nbrs.kneighbors(tfidf_matrix[content_index], n_neighbors=12)
-
-    # Collaborative-based recommendations
-    collab_index = data.index.get_loc(matched_game)
-    collab_similar_items = sorted(list(enumerate(collab_similarity_scores[collab_index])), key=lambda x: x[1], reverse=True)[1:12]
-
-    # Combine recommendations
     recommendations = {}
-    for idx, distance in zip(content_indices[0][1:], content_distances[0][1:]):
-        game = content_df.iloc[idx]['name']
-        recommendations[game] = recommendations.get(game, 0) + (1 - distance)
 
-    for idx, similarity in collab_similar_items:
-        game = data.index[idx]
-        recommendations[game] = recommendations.get(game, 0) + similarity
+    # Content-based recommendations (from content_df)
+    if matched_game in content_df['name'].values:
+        content_index = content_df[content_df['name'] == matched_game].index[0]
+        content_distances, content_indices = content_nbrs.kneighbors(tfidf_matrix[content_index], n_neighbors=12)
+        
+        # Collect content-based recommendations
+        for idx, distance in zip(content_indices[0][1:], content_distances[0][1:]):  # Skip the first as it’s the original game
+            game = content_df.iloc[idx]['name']
+            recommendations[game] = recommendations.get(game, 0) + (1 - distance)
+    # Collaborative-based recommendations (from collaborative data)
+    if game in data.index:
+        collab_index = data.index.get_loc(matched_game)
+        collab_similar_items = sorted(list(enumerate(collab_similarity_scores[collab_index])), key=lambda x: x[1], reverse=True)[1:12]
+        
+        # Collect collaborative-based recommendations
+        for idx, similarity in collab_similar_items:
+            game = data.index[idx]
+            recommendations[game] = recommendations.get(game, 0) + similarity
 
     ranked_recommendations = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)[:12]
     return matched_game, ranked_recommendations
